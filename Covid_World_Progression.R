9d7c33476d5897420e48ef0131913b8e7362969f
priyankasahoolyit <L00151175@student.lyit.ie>
2021-05-12 23:03:31 +0100

    WIP

--- Covid_World_Progression.R
+++ Covid_World_Progression.R
@@ -1,22 +1,36 @@
-# Created a new repository called CA1 in GitHub.
+# First, we created a new repository called CA1 in GitHub.
 # Then created a new project in R.
 # Get the current working directory
-getwd()
+# The working directory can be set if required
+
 setwd("C:\\Users\\deepa\\Documents\\R\\CA1")
-#  and reading it as a dataframe
+getwd()
+
+# We will load some libraries that we are going to use during the research.
+install.packages("ggplot2")   # Install package for Data visualization
+install.packages("mice")      # Install mice package and displayed the missing values
+install.packages("VIM")       # Install VIM package and displayed the missing values
+install.packages("psych")     # Install psych package to find correlations between multiple variables
+library(psych)
+library(VIM)
+library(ggplot2)
+library(mice)
+
 # File "covid.csv" downloaded from blackboard, 
 # storing the data file into the working directory and reading it as data frame called as "covid_data".
 # Covid data set holds lots of variables that contain an empty space.
 # Hence, replaced each empty space/missing content with NA.
+# Also, converting the character type into factor for better analysis.
 
-covid_data <- read.csv("covid.csv", na = "") # Reading covid.csv file
-covid_data [covid_data == ""] <- NA #Assigning blank spaces with NA
-head(covid_data, n = 15) # Display the first 15 records of the dataframe
-class(covid_data) # Confirm the class of covid_data
-str(covid_data) # Check the structure of data frame
-nrow(covid_data) #count the number of rows within the covid data frame
+covid_data <- read.csv("covid.csv", na = "", header = T, stringsAsFactors = T) # Reading covid.csv file
+covid_data [covid_data == ""] <- NA      # Assigning blank spaces with NA
+head(covid_data, n = 15)                 # Display the first 15 records of the dataframe
 
-#------------------------ Data Preparation ------------------------------------#
+class(covid_data)                        # Confirm the class of covid_data
+str(covid_data)                          # Check the structure of data frame
+nrow(covid_data)                         # Count the number of rows within the covid data frame 
+
+#------------------------------ Data Preparation -----------------------------------------------#
 
 # Structure displays that there are total 84529 observations and 59 variables in the Covid dataset.
 # The datetime field is converted to a date variable in mm/dd/yyyy format from chr variable.
@@ -25,25 +39,49 @@ converted_date <- as.Date(covid_data$date, "%d/%m/%Y") # datetime variable is co
 converted_date
 str(converted_date)
 
-#Counting the total number of `NA` values to see, 
-#how many null values was there in the entire dataset
-sum(is.na(covid_data$Count))
+# Getting the total number of `NA` values to see, how many null values were there in the entire dataset.
+# Finding which columns contain `NA` values
 
-#Viewing the records with NA
+sum(is.na(covid_data))                     # Count of `NA` is 2009585
+names(which(sapply(covid_data, anyNA)))    # Almost all the variables contains `NA`, 
+                                           # except `iso_code`, `location`, `date`
+
+# Viewing the records with NA
 na_records <- covid_data[!complete.cases(covid_data),]
 na_records
-#----------------------------------------------------------------------------
-#Research Question 1: Effect of the vaccine on New cases
+
+
+covid_data$new_cases[is.na(covid_data$new_cases)] <- 0
+covid_data$new_cases
+covid_data$people_fully_vaccinated[is.na(covid_data$people_fully_vaccinated)] <- 0
+covid_data$people_fully_vaccinated
+
+
+#-------------------------------------- Hypothesis testing --------------------------------------#
+# Research Question 1: Effect of the vaccine on New cases
 # H0 : There is no correlation between people_fully_vaccinated and new_cases
 # H1 : There is correlation between people_fully_vaccinated and new_cases
 
-# Analysing the variables used in each variable
+# Analyzing the variables used in null and alternate hypothesis
 # people_fully_vaccinated = continuous interval variable
 # new_cases = continuous interval variable
 #----------------------------------------------------
 
-covid_subset <- subset(covid_data, select = c(people_fully_vaccinated, new_cases))
-covid_subset
+# creating a subset of covid_data for convenient hypothesis testing
+
+covid_subset <- subset(covid_data, select = c(iso_code, location, date, people_fully_vaccinated, new_cases))
+str(covid_subset)
+sum(is.na(covid_subset))
+
+# Our subset data is now clean, with no missing values. 
+# An interesting graph would be a graph showing the total number of new cases per month, grouped by location. 
+# We have to transform our dataset, so that it is grouped by month and location.
+
+covid_new <- covid_subset %>% group_by(date, location) %>%
+  summarize(people_fully_vaccinated = sum(people_fully_vaccinated), new_cases = sum(new_cases)) 
+head(covid_new)
+
+
 
 #Using mice library to display NA values and its count
 install.packages("mice")
@@ -75,10 +113,10 @@ ggplot(covid_subset, aes(x=people_fully_vaccinated,y=new_cases))+ geom_point(col
 
 
 
-#plot(people_fully_vaccinated, new_cases, pch = 9, col= "lightblue",
-  #   main = "comparision of people_fully_vaccinated with new_cases",
-  #   xlab = "people_fully_vaccinated",
-   #  ylab = "new_cases")
+plot(people_fully_vaccinated, new_cases, pch = 9, col= "lightblue",
+     main = "comparision of people_fully_vaccinated with new_cases",
+     xlab = "people_fully_vaccinated",
+     ylab = "new_cases")
 
 
 #Visual analysis seems to indicate the data normally distributed
@@ -109,10 +147,11 @@ qqline(new_cases, col = "red")
 install.packages("psych")
 library(psych)
 
-covid_subset <- subset(covid_data, select = c(people_fully_vaccinated, new_cases))
-covid_subset
+covid_subset1 <- subset(covid_subset, select = c(people_fully_vaccinated, new_cases))
+str(covid_subset1)
+sum(is.na(covid_subset1))
 
-pairs.panels(covid_subset,
+pairs.panels(covid_subset1,
              smooth = TRUE, # If TRUE, draws loess smooths
              scale = FALSE, # If TRUE, scales the correlation text font    
              density = TRUE, # If TRUE, adds density plots and histograms    
@@ -130,9 +169,16 @@ pairs.panels(covid_subset,
 
 # Need to decide a test for calulating p-value
 #Pearson’s Correlation Coefficient 
+#Spearman’s Correlation Coefficient (also use for ordinal data) 
 
-??pearson
 
+corr1 <- cor.test(x=covid_subset1$people_fully_vaccinated, 
+                 y=covid_subset1$new_cases, method = 'spearman')
+corr1
+
+corr2 <- cor.test(x=covid_subset1$people_fully_vaccinated, 
+                 y=covid_subset1$new_cases, method = 'pearson')
+corr2
 #---------------------------------------------------------------------------------------------------------------------------------------------
 #Research Question 2: Does covid affect diabetic patients
 # H0 : There is no correlation between new_deaths and diabetes_prevalence 
